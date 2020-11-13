59d29dc03afb57ff4670ad5c573e48348acd1e32
carlos vasquez <cvasquezmu.3it@bancochile.cl>
2020-11-13 05:44:48 -0400

    look and feel

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,6 @@ router.get("/", async (req, res, next) => {
   if(req.query.buscar){
 
     const palabra = req.query.buscar;
-    console.log(Number(palabra));
 
     var condition = {};
     if(isNaN(Number(palabra))){
@@ -19,63 +18,49 @@ router.get("/", async (req, res, next) => {
     }
 
     const palindromo = isPalindromo(req.query.buscar);
-    console.log('checking: ' + req.query.buscar + '= ' + palindromo);
-
-
     let perPage = 9;
     let page = req.params.page || 1;
+    const countBusqueda = await Product.countDocuments(condition);
     const result = await Product
       .find(condition)
       .skip((perPage * page ) - perPage)
       .limit(perPage)
       .exec((err, productosIndex) => {
           if(err) return next(err);
-          Product.countDocuments((err, count) => {
               res.render("index", {
                 productosIndex,
                 current: page,
-                pages: Math.ceil(count/perPage),
+                pages: Math.ceil(countBusqueda/perPage),
                 buscar: palabra,
                 isPalindrome: palindromo
               })
-          })
+
         });
   }
   else{
     let perPage = 9;
     let page = req.params.page || 1;
+    const countBusqueda = await Product.countDocuments();
     const result = await Product
       .find({})
       .skip((perPage * page ) - perPage)
       .limit(perPage)
       .exec((err, productosIndex) => {
           if(err) return next(err);
-          Product.countDocuments((err, count) => {
             res.render("index", {
               productosIndex,
               current: page,
-              pages: Math.ceil(count/perPage),
+              pages: Math.ceil(countBusqueda/perPage),
               buscar: '',
               isPalindrome: false
             })
-          })
       });
   }
-
-
-
-});
-
-router.get("/productos", async (req, res) => {
-    const productos = await Product.find();
-    res.render("productos", {productos})
 });
 
 router.get("/:page", async (req, res, next) => {
-
   if(req.query.buscar){
     const palabra = req.query.buscar;
-    console.log(Number(palabra));
 
     var condition = {};
     if(isNaN(Number(palabra))){
@@ -86,60 +71,52 @@ router.get("/:page", async (req, res, next) => {
     }
 
     const palindromo = isPalindromo(req.query.buscar);
-    console.log('checking: ' + req.query.buscar + '= ' + palindromo);
-
-
     let perPage = 9;
     let page = req.params.page || 1;
+    const countBusqueda = await Product.countDocuments(condition);
     const result = await Product
       .find(condition)
       .skip((perPage * page ) - perPage)
       .limit(perPage)
       .exec((err, productosIndex) => {
           if(err) return next(err);
-          Product.countDocuments((err, count) => {
               res.render("index", {
                 productosIndex,
                 current: page,
-                pages: Math.ceil(count/perPage),
+                pages: Math.ceil(countBusqueda/perPage),
                 buscar: palabra,
                 isPalindrome: palindromo
               })
-          })
+
         });
   }
   else{
     let perPage = 9;
     let page = req.params.page || 1;
+    const countBusqueda = await Product.countDocuments();
     const result = await Product
       .find({})
       .skip((perPage * page ) - perPage)
       .limit(perPage)
       .exec((err, productosIndex) => {
           if(err) return next(err);
-          Product.countDocuments((err, count) => {
             res.render("index", {
               productosIndex,
               current: page,
-              pages: Math.ceil(count/perPage),
+              pages: Math.ceil(countBusqueda/perPage),
               buscar: '',
               isPalindrome: false
             })
-          })
       });
   }
-
-
-
-
 });
 
 function isPalindromo(str){
-  var re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
+  var re = /[\W_]/g;
   var lowRegStr = str.toLowerCase().replace(re, '');
   var reverseStr = lowRegStr.split('').reverse().join('');
 
-  return reverseStr === lowRegStr; // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+  return reverseStr === lowRegStr;
 }
 
 
