db969cb08be82752f7d4847b83ff1952fba3b825
carlos vasquez <cvasquezmu.3it@bancochile.cl>
2020-11-14 05:14:05 -0400

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,62 @@ const router = express.Router();
 
 const Product = require('../models/Product');
 
+router.get("/productosTest", async (req, res, next) => {
+  if(req.query.buscar){
+
+    const palabra = req.query.buscar;
+
+    var condition = {};
+    if(isNaN(Number(palabra))){
+      condition = {$or:[ {'brand':{$regex:'.*'+ palabra + '.*'}}, {'description':{$regex:'.*'+ palabra + '.*'}} ]};
+    }
+    else{
+      condition = {'id':palabra};
+    }
+
+    const palindromo = isPalindromo(req.query.buscar);
+    let perPage = 9;
+    let page = req.params.page || 1;
+    const countBusqueda = await Product.countDocuments(condition);
+    const result = await Product
+      .find(condition)
+      .skip((perPage * page ) - perPage)
+      .limit(perPage)
+      .exec((err, productosIndex) => {
+          if(err) return next(err);
+          const response = {
+            data: productosIndex,
+            totalrows: countBusqueda,
+          };
+          res.statusCode = 200;
+          res.json(response);
+          res.end();
+
+        });
+
+
+  }
+  else{
+    let perPage = 9;
+    let page = req.params.page || 1;
+    const countBusqueda = await Product.countDocuments();
+    const result = await Product
+      .find({})
+      .skip((perPage * page ) - perPage)
+      .limit(perPage)
+      .exec((err, productosIndex) => {
+          if(err) return next(err);
+          const response = {
+            data: productosIndex,
+            totalrows: countBusqueda,
+          };
+          res.statusCode = 200;
+          res.json(response);
+          res.end();
+      });
+  }
+});
+
 router.get("/", async (req, res, next) => {
 
   if(req.query.buscar){
@@ -27,14 +83,13 @@ router.get("/", async (req, res, next) => {
       .limit(perPage)
       .exec((err, productosIndex) => {
           if(err) return next(err);
-              res.render("index", {
-                productosIndex,
-                current: page,
-                pages: Math.ceil(countBusqueda/perPage),
-                buscar: palabra,
-                isPalindrome: palindromo
-              })
-
+          res.render("index", {
+            productosIndex,
+            current: page,
+            pages: Math.ceil(countBusqueda/perPage),
+            buscar: palabra,
+            isPalindrome: palindromo
+          })
         });
   }
   else{
@@ -111,6 +166,8 @@ router.get("/:page", async (req, res, next) => {
   }
 });
 
+
+
 function isPalindromo(str){
   var re = /[\W_]/g;
   var lowRegStr = str.toLowerCase().replace(re, '');
@@ -118,6 +175,8 @@ function isPalindromo(str){
 
   return reverseStr === lowRegStr;
 }
+
+
 
 
 module.exports = router;
